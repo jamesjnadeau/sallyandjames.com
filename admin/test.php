fed35b4461118bb0be48a6cf792e2f7720e83104
jamesjnadeau <james.nadeau@gmail.com>
2014-10-03 10:46:19 +0000

    using a bucket I made because this actually worked in one of my tests after waiting  along period of time

--- admin/test.php
+++ admin/test.php
@@ -1,8 +1,11 @@
 <?php
 
 use google\appengine\api\cloud_storage\CloudStorageTools;
-$GLOBALS['bucket'] = CloudStorageTools::getDefaultGoogleStorageBucketName();
-$file = "gs://".$GLOBALS['bucket']."/test.txt";
+//$bucket = CloudStorageTools::getDefaultGoogleStorageBucketName();
+$bucket = 'sallyandjames';
+
+$file = "gs://".$bucket."/test.txt";
+
 file_put_contents($file, 'This is a test, please work' );
 sleep(2);
 echo file_get_contents($file);
